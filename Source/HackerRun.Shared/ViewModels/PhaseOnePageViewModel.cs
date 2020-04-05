2953bc1a9fcfd0e0bbd2c14dc15f1a7707344810
jevonne <jevonne63@gmail>
2020-04-05 14:28:05 -0800

    options

--- Source/HackerRun.Shared/ViewModels/PhaseOnePageViewModel.cs
+++ Source/HackerRun.Shared/ViewModels/PhaseOnePageViewModel.cs
@@ -1,15 +1,53 @@
-﻿using Xamarin.Forms;
+﻿using HackerRun.Shared.Models;
+using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
 
 namespace HackerRun.Shared.ViewModels
 {
     public class PhaseOnePageViewModel : BaseViewModel
     {
+        public ObservableCollection<EmailOptions> Options { get; set; }
+
         public Command AppearingCommand => new Command(ExecuteAppearingCommand);
         public Command DisappearingCommand => new Command(ExecuteDisappearingCommand);
 
         public PhaseOnePageViewModel(INavigation navigation)
         {
             Navigation = navigation;
+            populateEmail();
+        }
+
+        private void populateEmail()
+        {
+            Options = new ObservableCollection<EmailOptions>()
+            {
+                new EmailOptions
+                {
+                    OptionTitle="The message is sent from a public email domain"
+                },
+
+                new EmailOptions
+                {
+                    OptionTitle="The domain name is misspelled"
+                },
+
+                new EmailOptions
+                {
+                    OptionTitle="The email is poorly written"
+                },
+
+                new EmailOptions
+                {
+                    OptionTitle="It includes suspicious attachments or links"
+                },
+
+                new EmailOptions
+                {
+                    OptionTitle="All of the above( correct answer)"
+                }
+            };
         }
 
         private void ExecuteAppearingCommand()
@@ -27,5 +65,10 @@ namespace HackerRun.Shared.ViewModels
         {
             //...
         }
+
+        
+        
+
+        //public List<string> Images { get; set; }
     }
 }
