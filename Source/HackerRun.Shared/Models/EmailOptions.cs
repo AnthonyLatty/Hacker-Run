eef30e439fc7255df64f92b6ffe2b7461ae0631f
jevonne <jevonne63@gmail>
2020-04-05 14:28:55 -0800

    options again

---
+++ Source/HackerRun.Shared/Models/EmailOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRun.Shared.Models
+{
+    public class EmailOptions
+    {
+        public string OptionTitle { get; set; }
+    }
+}
